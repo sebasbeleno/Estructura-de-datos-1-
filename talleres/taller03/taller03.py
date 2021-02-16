e6fd4f40b2c836b2b6f70e1767c66492cfe22843
jmmarulang <78051861+jmmarulang@users.noreply.github.com>
2021-02-16 16:58:46 -0500

    Update taller03.py

--- talleres/taller03/taller03.py
+++ talleres/taller03/taller03.py
@@ -5,7 +5,19 @@ def hanoi(topN, a = "Tower1", b = "Tower2", c = "Tower3"):
     hanoi(topN-1, a, c, b)
     print("Disco " + str(topN) + " de " + a + " a " + b)
     hanoi(topN-1, c,b , a)
-hanoi(4, "1", "3", "2")
-"""def subset(s, base = ""):
-  
-def permutations(base, stri):"""
+
+def subset(s, base = ""):
+    if len(s) == 0:
+        print(base)
+    else:
+        subset(s[1:], base + s[0])
+        subset(s[1:], base)
+
+def permutations(s, base = ""):
+    if len(s) == 0:
+        print (base)
+    else:
+        i = 0
+        while i < len(s):
+            permutations(s[0:i] + s[i+1:], base + s[i], )
+            i = i + 1
