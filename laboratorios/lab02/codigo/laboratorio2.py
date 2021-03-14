aad426e412ad185983ed44d3697c199d31836b54
Sebastian Beleño <sebasbeleno15@gmail.com>
2021-03-14 01:37:14 -0500

    Lab 2, inform and code

---
+++ laboratorios/lab02/codigo/laboratorio2.py
@@ -0,0 +1,71 @@
+import random
+import sys
+import time
+
+sys.setrecursionlimit(1000000)
+
+# Function to do insertion sort 
+def insertionSort(arr):
+	  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+"""
+Mohit Kumra (2020) Insertion Sort. [Source code] https://www.geeksforgeeks.org/python-program-for-insertion-sort/.
+"""
+
+# Python program for implementation of MergeSort
+def mergeSort(arr):
+	if len(arr) > 1:
+     
+         # Finding the mid of the array
+        mid = len(arr)//2
+ 
+        # Dividing the array elements
+        L = arr[:mid]
+ 
+        # into 2 halves
+        R = arr[mid:]
+ 
+        # Sorting the first half
+        mergeSort(L)
+ 
+        # Sorting the second half
+        mergeSort(R)
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+"""
+Mayank Khanna(2021) Merge Sort. [Source code] https://www.geeksforgeeks.org/merge-sort/.
+"""
